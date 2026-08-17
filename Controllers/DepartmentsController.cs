d1dfd31f4aa85eb862d20f9e6129689c4db7f9a3
Yousuf Ali Apel <yousufaliapel785@gmail.com>
2026-08-17 13:12:20 +0600

    Complete Patient and Doctor Management

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,48 @@
+﻿using HospitalManagementSystem.Data;
+using HospitalManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly HospitalDbContext _context;
+
+        public DepartmentsController(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Departments
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _context.Departments.ToListAsync();
+
+            return View(departments);
+        }
+
+        // GET: Departments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Add(department);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(department);
+        }
+    }
+}
---
+++ Controllers/DoctorsController.cs
@@ -0,0 +1,178 @@
+﻿using HospitalManagementSystem.Data;
+using HospitalManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly HospitalDbContext _context;
+
+        public DoctorsController(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Doctors
+        public async Task<IActionResult> Index()
+        {
+            var doctors = await _context.Doctors
+    .ToListAsync();
+
+            return View(doctors);
+        }
+
+        // GET: Doctors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctors
+    .FirstOrDefaultAsync(d => d.DoctorId == id);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+        // GET: Doctors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Doctors.Add(doctor);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(doctor);
+        }
+
+        // GET: Doctors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctors.FindAsync(id);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Departments = new SelectList(
+                _context.Departments,
+                "DepartmentId",
+                "DepartmentName",
+                doctor.DepartmentId
+            );
+
+            return View(doctor);
+        }
+
+        // POST: Doctors/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(
+    int id,
+    [Bind("DoctorId,FullName,Specialization,Qualification,Phone,Email,AvailableDays,AvailableTime,DepartmentId")]
+    Doctor doctor)
+        {
+            if (id != doctor.DoctorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(doctor);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DoctorExists(doctor.DoctorId))
+                    {
+                        return NotFound();
+                    }
+
+                    throw;
+                }
+            }
+
+            ViewBag.Departments = new SelectList(
+                _context.Departments,
+                "DepartmentId",
+                "DepartmentName",
+                doctor.DepartmentId
+            );
+
+            return View(doctor);
+        }
+
+        // GET: Doctors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctors
+    .FirstOrDefaultAsync(d => d.DoctorId == id);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DoctorExists(int id)
+        {
+            return _context.Doctors.Any(e => e.DoctorId == id);
+        }
+    }
+}
